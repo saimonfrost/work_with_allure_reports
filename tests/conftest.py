a834a024419f1f76364d0fd9018fc9b032171e07
Fedorov Semyon <fedorovedorov@gmail.com>
2023-11-28 00:28:54 +0300

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,5 +23,4 @@ def browser_setup():
     browser.config.driver = driver
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
     yield
